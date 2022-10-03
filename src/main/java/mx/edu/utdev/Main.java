020e61c883cf51b7baa59567448bba6b651475c4
Eduardo Rivas <eduardo.rivas.cuevas@outlook.com>
2022-10-03 13:15:01 -0500

    Last commit test

--- src/main/java/mx/edu/utdev/Main.java
+++ src/main/java/mx/edu/utdev/Main.java
@@ -2,6 +2,6 @@ package mx.edu.utdev;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world de Edriv!");
+        System.out.println("Last hello world!");
     }
 }
