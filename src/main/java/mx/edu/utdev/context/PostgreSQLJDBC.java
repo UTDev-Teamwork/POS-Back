bf5fe729d5c7a56e875f596881999e43fcd28468
Eduardo Rivas <eduardo.rivas.cuevas@outlook.com>
2022-10-06 10:55:09 -0500

    Javalin & DBConnection

---
+++ src/main/java/mx/edu/utdev/context/PostgreSQLJDBC.java
@@ -0,0 +1,23 @@
+package mx.edu.utdev.context;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class PostgreSQLJDBC {
+
+    public PostgreSQLJDBC() {
+        Connection c = null;
+        try {
+            Class.forName("org.postgresql.Driver");
+            c = DriverManager
+                    .getConnection("jdbc:postgresql://localhost:5432/pos_admin",
+                            "postgres", "1234");
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+            System.exit(0);
+        }
+        System.out.println("Opened database successfully");
+    }
+
+}
